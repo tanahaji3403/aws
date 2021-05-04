873f2f03e39beff26146bd7371051b36f9d6c7ef
EC2 Default User <ec2-user@ip-172-31-16-254.us-east-2.compute.internal>
2021-05-04 11:44:24 +0000

    udemy.rbを追加

---
+++ udemy.rb
@@ -0,0 +1,20 @@
+# puts "Hello Wasakara!!"
+
+# a = 1
+# b = 2
+# c = a + b
+
+# puts a
+# puts b
+# puts c
+# puts a + b
+
+# a = "Wasabi"
+# b = "Karashi"
+
+# puts a
+# puts b
+
+a = 123
+b = "The number is #{a}"
+puts b
